6a1204f85c8f76eee58e2dbba3c7e60da5943565
Sakshi Goraniya <sakshigoraniya13@gmail.com>
2022-02-24 12:17:27 +0530

    PHP assignment of Unit Conversions

---
+++ Converter/menu.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Converter</title>
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <link rel="stylesheet" h="style.css">
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+<div class="container">
+
+<h2>Vertical Button Groups</h2>
+
+<div style="float:left; width:120px;">
+<div class="btn-group-vertical">
+
+ <button  type="button" class="btn btn-info">Android</button>
+ <button type="button" class="btn btn-info">PHP</button> 
+ <button type="button" class="btn btn-info">Bootstrap</button>
+ <button type="button" class="btn btn-info">WordPress</button></div></br></br>
+</div>
+</body>
+</html>
